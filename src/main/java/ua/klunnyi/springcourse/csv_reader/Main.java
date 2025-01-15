1145db0fb454ecda63ed9a4ed381461925c1e52a
Serhii Klunnyi <serhii.klunnyi@appliedmethods.biz>
2025-01-15 07:15:37 +0200

    add refresh()

--- src/main/java/ua/klunnyi/springcourse/csv_reader/Main.java
+++ src/main/java/ua/klunnyi/springcourse/csv_reader/Main.java
@@ -1,11 +1,14 @@
 package ua.klunnyi.springcourse.csv_reader;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ua.klunnyi.springcourse.csv_reader.service.AppService;
 
 public class Main {
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("/context.xml");
+//        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        context.refresh();
         AppService bean = context.getBean(AppService.class);
         bean.start();
     }
